cc7f327b1b40e08ca18bb368a8b6927cf317407d
Boris Mirtskhulava <mircxu1898@gmail.com>
2019-03-23 14:27:57 +0400

    like/dislike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,4 +95,154 @@ router.delete(
   }
 );
 
+// @route  POST api/posts/like/:id
+// @desc   Like Post
+// @access Private
+
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already liked this post" });
+          } else if (
+            post.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length > 0
+          ) {
+            return res.status(400).json({
+              itisdisliked:
+                "User can't like post that he has disliked, undislike it first"
+            });
+          }
+
+          //Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "post not found" }));
+    });
+  }
+);
+
+// @route  POST api/posts/unlike/:id
+// @desc   Like Post
+// @access Private
+
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "You have not yet liked this post" });
+          }
+
+          //Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          //Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "post not found" }));
+    });
+  }
+);
+
+// @route  POST api/posts/dislike/:id
+// @desc   dislike Post
+// @access Private
+
+router.post(
+  "/dislike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadydisliked: "User already liked this post" });
+          } else if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res.status(400).json({
+              itisliked:
+                "User can't dislike post that he has liked, unlike it first"
+            });
+          }
+          //Add user id to dislikes array
+          post.dislikes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "post not found" }));
+    });
+  }
+);
+
+// @route  POST api/posts/undislike/:id
+// @desc   undislike Post
+// @access Private
+
+router.post(
+  "/undislike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.dislikes.filter(
+              dislike => dislike.user.toString() === req.user.id
+            ).length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notdisliked: "You have not yet disliked this post" });
+          }
+
+          //Get remove index
+          const removeIndex = post.dislikes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out of array
+          post.dislikes.splice(removeIndex, 1);
+
+          //Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "post not found" }));
+    });
+  }
+);
+
 module.exports = router;
